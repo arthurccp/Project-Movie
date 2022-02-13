126e8c375f420a2d0395f32f65dc9d40b760577e
Arthur Silva <arthutquirinos@gmail.com>
2022-02-13 15:25:06 -0300

    Inserting bottomSheet for keyword search

--- Project-Movie/View/ContentView.swift
+++ Project-Movie/View/ContentView.swift
@@ -11,26 +11,110 @@ import SDWebImageSwiftUI
 struct ContentView: View {
     
     @StateObject var viewModel = MoviewViewModel()
+    
+    @State var offset: CGFloat = 0
+    @State var translation: CGSize = CGSize(width: 0, height: 0)
+    @State var location   : CGPoint = CGPoint(x: 0, y: 0)
 
     var body: some View {
         NavigationView{
-            VStack{
-                SearchBar()
-                    Spacer()
                 ZStack{
-                    if let movies = viewModel.movies{
-                        MoviesListView(viewModel: viewModel, movies: movies)
-                    }else{
-                        loadingView()
+                    VStack{
+                        if let movies = viewModel.movies{
+                            MoviesListView(viewModel: viewModel, movies: movies)
+                        }else{
+                            loadingView()
+                        }
                     }
+                    .frame(maxHeight: .infinity)
+                    GeometryReader { reader in
+                        BottomSheet()
+                            .offset(y: reader.frame(in: .global).height-60)
+                            .offset(y: offset)
+                            .gesture(DragGesture().onChanged({ (value) in
+                                withAnimation{
+                                    translation = value.translation
+                                    location    = value.location
+                                    
+                                    if value.location.y > reader.frame(in: .global).midX{
+                                        if value.translation.height < 0 && offset > (-reader.frame(in: .global).height + 60){
+                                            offset = value.translation.height
+                                        }
+                                    }
+                                    if value.location.y < reader.frame(in: .global).midX{
+                                        if value.translation.height > 0 && offset < 0{
+                                            offset = (-reader.frame(in: .global).height + 60) + translation.height
+                                        }
+                                    }
+                                }
+                            }).onEnded({ (value) in
+                                withAnimation {
+                                    if value.startLocation.y > reader.frame(in: .global).midX{
+                                        if -value.translation.height > reader.frame(in: .global).midX{
+                                            offset = (-reader.frame(in: .global).height + 60)
+                                            return
+                                        }
+                                        
+                                        offset = 0
+                                    }
+                                    if value.startLocation.y > reader.frame(in: .global).midX{
+                                        if value.translation.height < reader.frame(in: .global).midX{
+                                        offset = (-reader.frame(in: .global).height + 60)
+                                        return
+                                    }
+                                        offset = 0
+                                }
+                            }
+                        }))
+                    }
+
                 }
-            }
             .navigationTitle("Popular Movies")
         }
         .onAppear(){
         viewModel.fetchData()
             }
             .padding()
+    }
+}
+
+
+
+struct BottomSheet: View {
+    
+    @State var text = ""
+    
+    var body: some View {
+        VStack{
+            Capsule()
+                .fill(Color(white: 0.95))
+                .frame(width: 50, height: 5)
+            
+            TextField("Pesquisar", text: $text)
+
+            ScrollView(.vertical, showsIndicators: false, content: {
+                LazyVStack(alignment: .leading){
+                    ForEach(1...30, id: \.self){
+                        Text("Ola \($0)")
+                    }
+                }
+            })
+        }
+        .padding(.horizontal, 20)
+        .padding(.bottom, 50)
+        .padding(.top, 20)
+        .background(BlurShape())
+        
+    }
+}
+
+
+struct BlurShape: UIViewRepresentable{
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: .systemMaterial))
+    }
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        
     }
 }
 
