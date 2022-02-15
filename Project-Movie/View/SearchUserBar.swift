1502227eeca03ff613594981afecb63ca9399fb6
arthurccp <66705949+arthurccp@users.noreply.github.com>
2022-02-15 10:56:42 -0300

    Update SearchUserBar.swift

--- Project-Movie/View/SearchUserBar.swift
+++ Project-Movie/View/SearchUserBar.swift
@@ -6,7 +6,7 @@ struct SearchUserBar: View {
 
     var body: some View {
         ZStack {
-            Color.yellow
+            Color.gray
             HStack {
                 TextField("Search User", text: $text)
                     .frame(height: 32)
